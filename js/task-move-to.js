d9070b0b7790792deb3bdbb89ca49629d55d9a15
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2024-11-22 19:20:36 +0100

    still working on board.js to communicate with django backend. removed console.log

--- js/task-move-to.js
+++ js/task-move-to.js
@@ -3,14 +3,12 @@ let currentDraggedElement;
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("find_task").addEventListener("keyup", function (event) {
         if (event.key === "Enter") {
-            //searchTask();
         }
     });
 });
 
 function startDragging(id) {
-    console.log(`Dragging gestartet: ${id}`);
-    currentDraggedElement = id; // ID des Elements speichern
+    currentDraggedElement = id; 
 }
 
 function allowDrop(ev) {
@@ -24,30 +22,21 @@ async function moveTo(bucket) {
     }
 
     try {
-        // PATCH-Request zum Backend senden
         const response = await fetch(`http://localhost:8000/api/tasks/${currentDraggedElement}/update/`, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ bucket })  // Task mit neuem "bucket" speichern
+            body: JSON.stringify({ bucket })  
         });
-
         if (!response.ok) {
             console.error('Fehler beim Aktualisieren:', response.status, response.statusText);
             return;
         }
-
-        console.log(`Task ${currentDraggedElement} erfolgreich verschoben nach ${bucket}`);
-
-        // Nach dem Update das Board mit den neuesten Daten neu laden
-        await loadAddedTasksFromStorage();  // Lädt die neuesten Tasks
-        loadBoard();  // Board aktualisieren
-
+        await loadAddedTasksFromStorage();  
+        loadBoard();  
     } catch (error) {
         console.error('Fehler beim Verschieben:', error);
     }
 }
-
-
 
 function highlight(id) {
     document.getElementById(id).classList.add("drag-area-highlight");
